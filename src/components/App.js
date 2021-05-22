34e025409e69dc108d7b4001d98ca2ee24493141
ilona <ilona.avdiushkina@gmail.com>
2021-05-22 14:09:37 +0200

    local connection to db

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,13 @@ import ListToBuy from "./ListToBuy";
 import ListChecked from "./ListChecked";
 import Footer from "./Footer";
 
+async function checkAPI() {
+    const res = await fetch('/items')
+    const data = await res.json()
+    console.log(data)
+}
+checkAPI()
+
 function App() {
     return (
         <div className="App">
